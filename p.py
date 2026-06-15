f451c48d1a11f69730649709e7107c2bfb113de6
kiranam850-afk <kiranam850@gmail.com>
2026-06-15 10:55:03 +0700

    test

--- p.py
+++
@@ -1,6 +0,0 @@
-c = float(input('Masukkan berapa nilai celcius: '))
-f = (c *9/5)+32
-a = int(c)
-print('\n') 
-print(f'celcius= {a}')
-print(f'Suhu {c} C sama dengan {f} F')                                 
